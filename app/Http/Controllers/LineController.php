c6dc18be597aefb27dbf280fba9997722b16ba0e
Jessie Xie <jessie75919@gmail.com>
2018-03-24 16:51:20 +0800

    add Some Setup

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use function config;
+use function dd;
 use Illuminate\Http\Request;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class LineController extends Controller
 {
     public function index () {
-        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(config('app.channel_token'));
-        $bot        = new \LINE\LINEBot($httpClient, ['channelSecret' => config('app.channel_secret')]);
+        $httpClient = new CurlHTTPClient(config('app.channel_token'));
+        $bot        = new LINEBot($httpClient, ['channelSecret' => config('app.channel_secret')]);
 
+        dd($httpClient);
 
 
         $response = $bot->replyText('<reply token>', 'hello!');
