7d99d886e0d9e642d5c34d94bf441852fdea5a56
Jessie Xie <jessie75919@gmail.com>
2019-10-19 22:14:44 +0800

    add: add validate signature in LineMessageApiRequest.php

---
+++ app/Http/Requests/Line/LineMessageApiRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests\Line;
+
+use Illuminate\Foundation\Http\FormRequest;
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\Exception\InvalidSignatureException;
+use LogicException;
+
+class LineMessageApiRequest extends FormRequest
+{
+    /**
+     * @var LINEBot
+     */
+    private $lineBot;
+
+    /**
+     * LineMessageApiRequest constructor.
+     * @param  LINEBot  $lineBot
+     */
+    public function __construct(LINEBot $lineBot)
+    {
+        parent::__construct();
+        $this->lineBot = $lineBot;
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     * @return bool
+     */
+    public function authorize()
+    {
+        try {
+            return $this->lineBot->validateSignature(
+                $this->getContent(),
+                $this->header(HTTPHeader::LINE_SIGNATURE)
+            );
+        } catch (InvalidSignatureException $e) {
+            return false;
+        } catch (LogicException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
+}
