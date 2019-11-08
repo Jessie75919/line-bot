039df94959e130363e675a9f83c015cd4f8cde32
Jessie Xie <jessie75919@gmail.com>
2019-11-09 02:00:59 +0800

    md: use method to get data in LocationTypePayloadHandler.php

--- app/Services/LineBot/TypePayloadHandler/LocationTypePayloadHandler.php
+++ app/Services/LineBot/TypePayloadHandler/LocationTypePayloadHandler.php
@@ -3,15 +3,13 @@
 namespace App\Services\LineBot\TypePayloadHandler;
 
 use App\Services\LineBot\ActionHandler\LineBotActionFoodNearbySearcher;
-use App\Services\LineBot\ActionHandler\LineBotCommandHelper;
+use LINE\LINEBot\Event\MessageEvent\LocationMessage;
 
 class LocationTypePayloadHandler implements TypePayloadHandlerInterface
 {
     const NEARBY_SEARCH = 'nearby_search';
     private $memory;
-    private $rawPayload;
     private $payload;
-    /** * @var string */
     private $purpose;
 
     /**
@@ -23,22 +21,21 @@ class LocationTypePayloadHandler implements TypePayloadHandlerInterface
         $this->memory = $memory;
     }
 
-    public function checkPurpose($payload)
+    public function checkPurpose($messageEvent)
     {
-        $this->rawPayload = $payload;
         $this->purpose = self::NEARBY_SEARCH;
         return $this;
     }
 
-    public function preparePayload()
+    public function preparePayload(LocationMessage $messageEvent)
     {
         $this->payload = [
             'channelId' => $this->memory->channel_id,
             'purpose' => $this->purpose,
-            'origin' => $this->rawPayload,
-            'address' => $this->rawPayload['address'],
-            'latitude' => $this->rawPayload['latitude'],
-            'longitude' => $this->rawPayload['longitude'],
+            'origin' => $messageEvent,
+            'address' => $messageEvent->getAddress(),
+            'latitude' => $messageEvent->getLatitude(),
+            'longitude' => $messageEvent->getLongitude(),
         ];
         return $this;
     }
@@ -49,10 +46,6 @@ class LocationTypePayloadHandler implements TypePayloadHandlerInterface
             case self::NEARBY_SEARCH:
                 return new LineBotActionFoodNearbySearcher($this->payload);
                 break;
-
-            default:
-                $this->payload['purpose'] = 'help';
-                return new LineBotCommandHelper($this->payload);
         }
     }
 }
