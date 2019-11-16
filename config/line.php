b405b381c0ca1a0abbf1dd0fd5db3441293502e0
Jessie Xie <jessie75919@gmail.com>
2019-11-17 01:41:00 +0800

    add: add config for LINE_LINK_OF_WEIGHT_INPUT

--- config/line.php
+++ config/line.php
@@ -4,5 +4,6 @@ return [
     'liff_token' => env("LINE_LIFF_TOKEN"),
     'channel_token' => env('CHANNEL_TOKEN'),
     'channel_secret' => env('CHANNEL_SECRET'),
+    'link_of_weight_input' => env('LINE_LINK_OF_WEIGHT_INPUT'),
 ];
 
