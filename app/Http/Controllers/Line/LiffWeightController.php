4d3beafbcf99ba63c0802d060ff1e70a6d3f36e4
Jessie Xie <jessie75919@gmail.com>
2019-11-17 22:22:40 +0800

    add: add codes related to Setting

--- app/Http/Controllers/Line/LiffWeightController.php
+++ app/Http/Controllers/Line/LiffWeightController.php
@@ -3,17 +3,36 @@
 namespace App\Http\Controllers\Line;
 
 use App\Http\Controllers\Controller;
+use App\Models\Memory;
+use App\Models\Weight;
 
 class LiffWeightController extends Controller
 {
     public function input()
     {
         return view(
-            'line.line-liff',
+            'line.input',
             [
-                'liffToken' => config('line.liff_token'),
+                'liffToken' => Weight::getPageToken(config('line.link_of_weight_input')),
                 'today' => now('Asia/Taipei')->toDateString(),
             ]
         );
+    }
+
+    public function setting()
+    {
+        return view(
+            'line.setting',
+            [
+                'liffToken' => Weight::getPageToken(config('line.link_of_weight_setting')),
+                'today' => now('Asia/Taipei')->toDateString(),
+            ]
+        );
+    }
+
+    public function mySetting(string $userId)
+    {
+        $memory = Memory::getByChannelId($userId);
+        return response()->json(['setting' => $memory->weightSetting]);
     }
 }
--- config/line.php
+++ config/line.php
@@ -5,5 +5,6 @@ return [
     'channel_token' => env('CHANNEL_TOKEN'),
     'channel_secret' => env('CHANNEL_SECRET'),
     'link_of_weight_input' => env('LINE_LINK_OF_WEIGHT_INPUT'),
+    'link_of_weight_setting' => env('LINE_LINK_OF_WEIGHT_SETTING'),
 ];
 
