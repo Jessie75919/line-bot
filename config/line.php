d7d6febdbda256f76d225685ad10cc78364a163b
Jessie Xie <jessie75919@gmail.com>
2019-11-17 23:52:12 +0800

    rm: remove unused codes

--- config/line.php
+++ config/line.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'liff_token' => env("LINE_LIFF_TOKEN"),
     'channel_token' => env('CHANNEL_TOKEN'),
     'channel_secret' => env('CHANNEL_SECRET'),
     'link_of_weight_input' => env('LINE_LINK_OF_WEIGHT_INPUT'),
