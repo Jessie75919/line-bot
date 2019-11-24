c06bba0cceff6d748d0f8c4a41f4cd05d6b42c0a
Jessie Xie <jessie75919@gmail.com>
2019-11-24 18:16:11 +0800

    md: use bootstrap card component

--- resources/assets/js/components/line/pages/WeightInput.vue
+++ resources/assets/js/components/line/pages/WeightInput.vue
@@ -1,28 +1,34 @@
 <template>
 	<div class="container mt-3">
-		<h3>今日：{{today}}</h3>
-		<form>
-			<div class="form-group-lg">
-				<label for="weight">體重（kg）</label>
-				<input class="form-control" id="weight" type="number" v-model="bodyStatus.weight">
+		<div class="card">
+			<div class="card-header text-white bg-dark">
+				{{today}}
 			</div>
-			<div class="form-group-lg mt-3">
-				<label for="fat">體脂（%）</label>
-				<input class="form-control" id="fat" type="number" v-model="bodyStatus.fat">
+			<div class="card-body">
+				<form>
+					<div class="form-group-lg">
+						<label for="weight">體重（kg）</label>
+						<input class="form-control" id="weight" type="number" v-model="bodyStatus.weight">
+					</div>
+					<div class="form-group-lg mt-3">
+						<label for="fat">體脂（%）</label>
+						<input class="form-control" id="fat" type="number" v-model="bodyStatus.fat">
+					</div>
+					<div class="form-group-lg mt-3">
+						<label for="fat">BMI (身高：{{setting.height}}cm 計算) </label>
+						<input class="form-control" id="bmi" readonly type="number" v-model="bmi">
+					</div>
+					<div class="form-group-lg text-center">
+						<button :disabled="isInputReady"
+						        @click.prevent="submit"
+						        class="btn btn-primary btn-lg mt-4 w-100"
+						        type="button">
+							記錄
+						</button>
+					</div>
+				</form>
 			</div>
-			<div class="form-group-lg mt-3">
-				<label for="fat">BMI (身高：{{setting.height}}cm 計算) </label>
-				<input class="form-control" id="bmi" readonly type="number" v-model="bmi">
-			</div>
-			<div class="form-group-lg text-center">
-				<button :disabled="isInputReady"
-				        @click.prevent="submit"
-				        class="btn btn-primary btn-lg mt-4 w-100"
-				        type="button">
-					記錄
-				</button>
-			</div>
-		</form>
+		</div>
 	</div>
 </template>
 
--- resources/assets/js/components/line/pages/WeightSetting.vue
+++ resources/assets/js/components/line/pages/WeightSetting.vue
@@ -1,68 +1,93 @@
 <template>
 	<div class="container mt-3">
-		<h3>設定</h3>
-		<form>
-			<div class="form-group-lg">
-				<label for="height">身高（cm）</label>
-				<input class="form-control" id="height" type="number" v-model="setting.height">
+		<div class="card">
+			<div class="card-header text-white bg-dark">
+				目標設定
 			</div>
-			<div class="form-group-lg mt-3">
-				<label for="weight">目標體重（kg）</label>
-				<input class="form-control" id="weight" type="number" v-model="setting.goal_weight">
-			</div>
-			<div class="form-group-lg mt-3">
-				<label for="fat">目標體脂（%）</label>
-				<input class="form-control" id="fat" type="number" v-model="setting.goal_fat">
-			</div>
-			<div class="form-group-lg mt-3">
-				<div class="form-check">
-					<input class="form-check-input" id="enable_notification"
-					       type="checkbox"
-					       v-model="setting.enable_notification">
-					<label class="form-check-label" for="enable_notification">
-						開啓記錄提醒
-					</label>
-				</div>
-			</div>
-			<transition name="fade">
-				<div v-if="setting.enable_notification">
+			<div class="card-body">
+				<form>
 					<div class="form-group-lg mt-3">
-						<label for="notify_day">每週提醒日</label>
-						<select class="form-control" id="notify_day" v-model="setting.notify_day">
-							<option value="1">星期一</option>
-							<option value="2">星期二</option>
-							<option value="3">星期三</option>
-							<option value="4">星期四</option>
-							<option value="5">星期五</option>
-							<option value="6">星期六</option>
-							<option value="0">星期日</option>
-						</select>
+						<label for="weight">目標體重（kg）</label>
+						<input class="form-control" id="weight" type="number" v-model="setting.goal_weight">
 					</div>
 					<div class="form-group-lg mt-3">
-						<label>提醒時間</label>
-						<div class="time-picker-container">
-							<select class="form-control time-picker"
-							        id="hour"
-							        v-model="time.hour">
-								<option :value="hour" v-for="hour in hours">{{hour}} 點</option>
-							</select>
-							<select class="form-control time-picker" id="minute" v-model="time.minute">
-								<option value="00">00 分</option>
-								<option value="30">30 分</option>
-							</select>
+						<label for="fat">目標體脂（%）</label>
+						<input class="form-control" id="fat" type="number" v-model="setting.goal_fat">
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="card mt-3">
+			<div class="card-header text-white bg-dark">
+				個人資料設定
+			</div>
+			<div class="card-body">
+				<form>
+					<div class="form-group-lg">
+						<label for="height">身高（cm）</label>
+						<input class="form-control" id="height" type="number" v-model="setting.height">
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="card mt-3">
+			<div class="card-header text-white bg-dark">
+				提醒設定
+			</div>
+			<div class="card-body">
+				<form class="mb-3">
+					<div class="form-group-lg mt-3">
+						<div class="form-check">
+							<input class="form-check-input" id="enable_notification"
+							       type="checkbox"
+							       v-model="setting.enable_notification">
+							<label class="form-check-label" for="enable_notification">
+								開啓記錄提醒
+							</label>
 						</div>
 					</div>
-				</div>
-			</transition>
-			<div class="form-group-lg text-center">
-				<button :disabled="! isSettingReady"
-				        @click.prevent="submit"
-				        class="btn btn-primary btn-lg mt-4 w-100"
-				        type="button">
-					設定
-				</button>
+					<transition name="fade">
+						<div v-if="setting.enable_notification">
+							<div class="form-group-lg mt-3">
+								<label for="notify_day">每週提醒日</label>
+								<select class="form-control" id="notify_day" v-model="setting.notify_day">
+									<option value="1">星期一</option>
+									<option value="2">星期二</option>
+									<option value="3">星期三</option>
+									<option value="4">星期四</option>
+									<option value="5">星期五</option>
+									<option value="6">星期六</option>
+									<option value="0">星期日</option>
+								</select>
+							</div>
+							<div class="form-group-lg mt-3">
+								<label>提醒時間</label>
+								<div class="time-picker-container">
+									<select class="form-control time-picker"
+									        id="hour"
+									        v-model="time.hour">
+										<option :value="hour" v-for="hour in hours">{{hour}} 點</option>
+									</select>
+									<select class="form-control time-picker" id="minute" v-model="time.minute">
+										<option value="00">00 分</option>
+										<option value="30">30 分</option>
+									</select>
+								</div>
+							</div>
+						</div>
+					</transition>
+
+				</form>
 			</div>
-		</form>
+		</div>
+		<div class="form-group-lg text-center mb-3">
+			<button :disabled="! isSettingReady"
+			        @click.prevent="submit"
+			        class="btn btn-primary btn-lg mt-4 w-100"
+			        type="button">
+				設定
+			</button>
+		</div>
 	</div>
 </template>
 
