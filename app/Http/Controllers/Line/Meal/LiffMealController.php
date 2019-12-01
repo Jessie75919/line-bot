f7b236e42e59d3440afc7e7d76d27e5742cec110
Jessie Xie <jessie75919@gmail.com>
2019-12-01 15:49:43 +0800

    add: add LineMealController

---
+++ app/Http/Controllers/Line/Meal/LiffMealController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Line\Meal;
+
+use App\Http\Controllers\Controller;
+use app\Services\LineBot\Liff\LiffService;
+use Illuminate\Http\Request;
+
+class LiffMealController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view(
+            "line.meal.index",
+            [
+                'liffToken' => LiffService::parsePageToken(config('line.link_of_weight_index')),
+                'today' => now('Asia/Taipei')->toDateString(),
+                'page' => $request->all()['page'] ?? 'index',
+            ]
+        );
+    }
+}
