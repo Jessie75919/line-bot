851a2f7fcafc5ed7e344762d4e382c254f5dbf6a
Jessie Xie <jessie75919@gmail.com>
2019-12-01 15:41:32 +0800

    add: add LINE_LINK_OF_MEAL_INDEX=

--- config/line.php
+++ config/line.php
@@ -4,5 +4,6 @@ return [
     'channel_token' => env('CHANNEL_TOKEN'),
     'channel_secret' => env('CHANNEL_SECRET'),
     'link_of_weight_index' => env('LINE_LINK_OF_WEIGHT_INDEX'),
+    'link_of_meal_index' => env('LINE_LINK_OF_MEAL_INDEX'),
 ];
 
