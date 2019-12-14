db98129980300850dbaa74fe6ccb388e3b9a7064
Jessie Xie <jessie75919@gmail.com>
2019-12-15 00:36:54 +0800

    add: add getMealQuickReplyButtons method in MealType

--- app/Models/Line/MealType.php
+++ app/Models/Line/MealType.php
@@ -3,10 +3,25 @@
 namespace App\Models\Line;
 
 use Illuminate\Database\Eloquent\Model;
+use LINE\LINEBot\QuickReplyBuilder\ButtonBuilder\QuickReplyButtonBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
 
 class MealType extends Model
 {
     protected $fillable = ['key', 'name', 'time'];
+
+    public static function getMealQuickReplyButtons()
+    {
+        return MealType::all()->map(function ($mealType) {
+            return new QuickReplyButtonBuilder(
+                new PostbackTemplateActionBuilder(
+                    $mealType->name,
+                    "meal，select，{$mealType->id}",
+                    "hi，我想要記錄{$mealType->name} 🙂️"
+                )
+            );
+        })->all();
+    }
 
     public function mealReminders()
     {
