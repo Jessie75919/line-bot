a9132be2dba7fea52fe57279842717267a1fbeb6
Jessie Xie <jessie75919@gmail.com>
2019-12-15 11:37:24 +0800

    md: move getMealQuickReplyButtons to LineBotMealMenu.php

--- app/Services/LineBot/ActionHandler/Meal/LineBotMealMenu.php
+++ app/Services/LineBot/ActionHandler/Meal/LineBotMealMenu.php
@@ -3,13 +3,28 @@
 namespace App\Services\LineBot\ActionHandler\Meal;
 
 use App\Jobs\Line\Meal\DeleteProcessStatus;
-use App\Models\Line\MealType;
 use App\Models\Line\ProcessStatus;
+use Illuminate\Database\Eloquent\Model;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\QuickReplyBuilder\ButtonBuilder\QuickReplyButtonBuilder;
 use LINE\LINEBot\QuickReplyBuilder\QuickReplyMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
 
 class LineBotMealMenu
 {
+    public function getMealQuickReplyButtons()
+    {
+        return Model::all()->map(function ($mealType) {
+            return new QuickReplyButtonBuilder(
+                new PostbackTemplateActionBuilder(
+                    $mealType->name,
+                    "meal，select，{$mealType->id}",
+                    "hi，我想要記錄{$mealType->name} 🙂️"
+                )
+            );
+        })->all();
+    }
+
     public function startMenu(ProcessStatus $processStatus)
     {
         $processStatus->mealStart();
@@ -24,7 +39,7 @@ class LineBotMealMenu
      */
     public function getMealTypeQuickMenus(string $text): TextMessageBuilder
     {
-        $mealsBtns = MealType::getMealQuickReplyButtons();
+        $mealsBtns = $this->getMealQuickReplyButtons();
         return new TextMessageBuilder(
             $text,
             new QuickReplyMessageBuilder($mealsBtns)
